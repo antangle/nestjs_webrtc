92307b17423d7d8d6a11e7680274a76516498319
antangle <zanaris@naver.com>
2022-09-20 16:55:16 +0900

    typeorm 연동

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,54 @@
+import { Auth } from './auth.entity';
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToOne, JoinColumn} from 'typeorm/index';
+
+@Entity('user')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id?: number;
+
+    @Column({
+        unique: true
+    })
+    username!: string;
+
+    @Column({
+        unique: true
+    })
+    email?: string;
+
+    @Column({
+        type: 'varchar',
+        length: 288
+    })
+    password!: string;
+
+    @Column()
+    nickname?: string;
+
+    @CreateDateColumn()
+    created_at?: Date;
+
+    @UpdateDateColumn()
+    updated_at?: Date;
+
+    @Column({
+        nullable: true
+    })
+    start_of_day?: string;
+
+    @Column({
+        nullable: true
+    })
+    end_of_day?: string;
+
+    @Column({
+        default: 0
+    })
+    terms_and_agreement?: number;
+
+    @OneToOne(() => Auth)
+    @JoinColumn()
+    auth?: Auth;
+
+}
